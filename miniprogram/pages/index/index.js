3565a60223b9a4a4efd7e1e1738f03681e61a974
liangwei <banemail@gmail.com>
2023-02-03 15:59:22 +0800

    16-完成通告列表的渲染

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,9 +1,21 @@
 
 Page({
+  data: {
+    // 储存数据
+    notifyList: []
+  },
+
+  // 进入页面, 调用获取函数
   async onLoad() {
-    const res = await wx.http({
-      url: 'https://cnodejs.org/api/v1/topics'
+    this.getNotifyList()
+  },
+
+  // 获取列表的函数封装
+  async getNotifyList() {
+    const {code, data: notifyList} = await wx.http.get('/announcement')
+    if(code!==10000) return wx.utils.toast()
+    this.setData({
+      notifyList
     })
-    console.log(res);
   }
 })
--- miniprogram/utils/http.js
+++ miniprogram/utils/http.js
@@ -1,7 +1,7 @@
 import http from 'wechat-http'
 
 // 设定baseURL
-http.baseURL = 'https://live-api.itheima.net/'
+http.baseURL = 'https://live-api.itheima.net'
 
 // 设定拦截器
 http.intercept.response = res => {
