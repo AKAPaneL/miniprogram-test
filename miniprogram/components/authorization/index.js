225d8c49f6d7ecae1904867bc2a4841033b34d3b
liangwei <banemail@gmail.com>
2023-02-05 10:00:54 +0800

    05-跳转登录页前记录当前页地址

--- miniprogram/components/authorization/index.js
+++ miniprogram/components/authorization/index.js
@@ -24,14 +24,27 @@ Component({
   lifetimes: {
     attached() {
       // 取得当前的 token 转换为布尔值赋值给 isLogin
-      const isLogin = !!getApp().token
+      // const isLogin = !!getApp().token
+      // 上面这句代码没有对token进行修改, 只是用token就得到一个 true 没有就得到一个 false
+      // 还可以写成
+      const token = getApp().token
+      const isLogin = Boolean(token)
+
       this.setData({
         isLogin
       })
       // 如果发现没有登录, 跳转到登录页
       if(!isLogin) {
+
+        const pageStack = getCurrentPages()
+        console.log('页面数组', pageStack);
+        const currentPage = pageStack[pageStack.length - 1]
+        console.log('当前页面', currentPage);
+        const url = currentPage.route
+        console.log('当前页面地址', url);
+
         wx.redirectTo({
-          url: '/pages/login/index'
+          url: `/pages/login/index?redirectURL=/${url}`
         })
       }
     }
