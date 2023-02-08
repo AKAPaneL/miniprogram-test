73334d0cc8eff1b314608a17dafa47ad4e8dd94c
AKAPaneL <dpanzi@outlook.com>
2023-02-08 18:07:50 +0800

    获取房屋详情

--- miniprogram/house_pkg/pages/detail/index.js
+++ miniprogram/house_pkg/pages/detail/index.js
@@ -1,9 +1,17 @@
 Page({
-  onLoad({ id }) {
-    console.log(id);
+  data: {
+    houseDetail: {}
   },
-  getHouseDetail(id) {
-
+  onLoad({ id }) {
+    this.getHouseDetail(id)
+  },
+  // 根据id获取对应房屋的详情
+  async getHouseDetail(id) {
+    if (!id) return
+    const res = await wx.http.get(`/room/${id}`)
+    this.setData({
+      houseDetail: res.data
+    })
   },
   editHouse() {
     wx.navigateTo({
