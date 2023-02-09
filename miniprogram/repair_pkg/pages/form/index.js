95e633d3730f2aa1bccda1a4f6a0a60de1a38bc8
AKAPaneL <dpanzi@outlook.com>
2023-02-09 12:12:54 +0800

    添加保修列表

--- miniprogram/repair_pkg/pages/form/index.js
+++ miniprogram/repair_pkg/pages/form/index.js
@@ -10,42 +10,50 @@ Page({
       { name: '北京育新花园3号楼 703' },
       { name: '北京天通苑北苑8号楼 403' },
     ],
-    houseSelected: '',
-    repairSelected: '',
-    dateSelected: '',
+    houseSelected: {},
+    repairSelected: {},
+    appointment: '',
     mobile: '',
     description: '',
-    repairItem: [{ name: '水路卫浴' }, { name: '电路灯具' }, { name: '管道疏通' }, { name: '开锁换锁' }],
+    repairItem: [],
     fileList: []
   },
   onLoad() {
     this.loadHouseList()
+    this.loadRepairItem()
   },
   // 选择房屋
   onSelectHouse(e) {
     this.setData({
-      houseSelected: e.detail.name
+      houseSelected: e.detail
     })
   },
   // 选择维修项目
   onSelectRepair(e) {
     this.setData({
-      repairSelected: e.detail.name
+      repairSelected: e.detail
     })
   },
   // 选择时间
   onInput(e) {
     this.setData({
       currentDate: e.detail,
-      dateSelected: new Date(e.detail).toLocaleString()
+      appointment: new Date(e.detail).toLocaleString()
     });
-    console.log(this.data.dateSelected);
     this.closeDateLayer()
   },
+  // 获取房屋列表
   async loadHouseList() {
     const res = await wx.http.get('/house')
     this.setData({
       houseList: res.data
+    })
+  },
+  // 获取维修项目
+  async loadRepairItem() {
+    const res = await wx.http.get('/repairItem')
+    this.setData({
+      repairItem: res.data
     })
   },
   // 选择文件之后的回调 callback
@@ -77,8 +85,20 @@ Page({
     })
   },
   // 提交表单
-  submit() {
-
+  async submit(e) {
+    // 表单验证...省略
+    //获取数据
+    const { houseSelected: { id: houseId }, repairSelected: { id: repairItemId }, appointment, mobile, description, fileList: attachment } = this.data
+    const res = await wx.http.post('/repair', {
+      houseId,
+      repairItemId,
+      appointment,
+      mobile,
+      description,
+      attachment
+    })
+    if (code !== 10000) return wx.showToast({ title: '在线报修失败!', icon: 'none' })
+    this.goList()
   },
   openHouseLayer() {
     this.setData({ houseLayerVisible: true })
